cfa8d5e893297496513646e2c6f031e041fb2e29
An <anknguyenanknguyen@gmail.com>
2022-10-22 12:49:29 -0700

    messing around

--- make_calendar.R
+++ make_calendar.R
@@ -1,6 +1,7 @@
 library(tidyverse)
+library(ggpath)
 
-tracker <- readRDS("data/transitive_tracker_2021_22.rds")
+tracker <- readRDS("data/transitive_tracker_an_2021_22.rds")
 
 tracker <- tracker |>
   padr::pad(start_val = as.Date("2021-10-01")) |>
@@ -28,11 +29,27 @@ chart <- tracker |>
   left_join(
     hockeyR::team_logos_colors |>
       mutate(full_team_name = ifelse(team_abbr == "MTL","Montréal Canadiens", full_team_name)),
-    by = c("team" = "full_team_name"))
+    by = c("team" = "full_team_name")) |>
+  left_join(
+    hockeyR::team_logos_colors |>
+      mutate(full_team_name = ifelse(team_abbr == "MTL","Montréal Canadiens", full_team_name)),
+    by = c("opponent" = "full_team_name"), suffix=c("","_opponent")) |>
+  mutate(
+    angle = runif(n(),min=-15,max=15),
+    angle_opponent = runif(n(),min=-15,max=15)
+  )
 
 transparent <- function(img) {
   magick::image_fx(img, expression = "0.8*a", channel = "alpha")
 }
+
+image_mod <- function(img) {
+  # edge <- magick::image_canny(img, '5x5+15%+100%')
+  # return(magick::image_mosaic(c(edge, img)) |> magick::image_fill("none"))
+  
+  return(magick::image_rotate(img,runif(1,min=-15,max=15))|> magick::image_fill("none"))
+}
+
 
 chart |>
   ggplot(aes(weekday, week_num)) +
@@ -42,10 +59,20 @@ chart |>
     nudge_x = .5, nudge_y = .5,
     hjust = 1, vjust = 1
     ) +
-  ggimage::geom_image(
-    aes(image = team_logo_espn),
-    size = .075, asp = 1.5,
-    image_fun = transparent
+  ggpath::geom_from_path(
+    data = chart |> filter(cup_change),
+    aes(path = team_logo_espn_opponent, angle=angle_opponent),
+    width = .075
+  ) +
+  ggpath::geom_from_path(
+    aes(path = team_logo_espn, angle=angle),
+    width = .075
+  ) +
+  geom_text(
+    data = chart |> drop_na(team_score),
+    aes(label = paste(team_score,opponent_score,sep="-")),
+    face = "bold",
+    size = 1.75, vjust = 0,  hjust = 1,  nudge_x = .5, nudge_y = -.45,
   ) +
   facet_wrap(~month, nrow = 3) +
   scale_y_reverse() +
@@ -68,4 +95,4 @@ chart |>
     title = "The Transitive Stanley Cup",
     subtitle = "2021-22 NHL Season"
   )
-ggsave("figures/transitive_cup_calendar.png", width = 9, height = 6)
+ggsave("figures/transitive_cup_calendar_an.png", width = 9, height = 6)
--- transitive_tracker.R
+++ transitive_tracker.R
@@ -2,14 +2,16 @@
 
 # get current year
 month <- as.numeric(format(Sys.Date(), "%m"))
-if(month >= 10){
-  current_season <- as.numeric(format(Sys.Date(), "%Y"))+1
-} else{
+if (month >= 10) {
+  current_season <- as.numeric(format(Sys.Date(), "%Y")) + 1
+} else {
   current_season <- as.numeric(format(Sys.Date(), "%Y"))
 }
 
+current_season = 2022
+
 # find last year's cup winner
-last_year <- hockeyR::get_game_ids(season = current_season-1) |>
+last_year <- hockeyR::get_game_ids(season = current_season - 1) |>
   dplyr::filter(game_type == "POST") |>
   dplyr::arrange(desc(date)) |>
   dplyr::slice(1)
@@ -17,12 +19,14 @@ last_year <- hockeyR::get_game_ids(season = current_season-1) |>
 games <- hockeyR::get_game_ids(season = current_season)
 
 games <- games |>
-  dplyr::filter(date <= Sys.Date() & game_type == "REG") |>
+  dplyr::filter(date <= Sys.Date() & game_type == "REG" & !(home_final_score == 0 & away_final_score == 0)) |>
   dplyr::arrange(date)
 
-if(nrow(games) > 0){
-  
+if (nrow(games) > 0) {
   double_games <- games |>
+    dplyr::mutate(
+      home_team = home_name
+    ) |>
     dplyr::rename(
       team = home_name,
       team_score = home_final_score,
@@ -31,19 +35,22 @@ if(nrow(games) > 0){
     ) |>
     dplyr::bind_rows(
       games |>
+        dplyr::mutate(
+          home_team = home_name
+        ) |>
         dplyr::rename(
           team = away_name,
           team_score = away_final_score,
           opponent = home_name,
-          opponent_score = home_final_score
+          opponent_score = home_final_score,
         )
     ) |>
     dplyr::mutate(date = as.Date(date)) |>
-    dplyr::arrange(date) |>
-    dplyr::mutate(win = ifelse(team_score > opponent_score, 1, 0))
-  
+    dplyr::arrange(date, home_team) |>
+    dplyr::mutate(win = ifelse(team_score > opponent_score, TRUE, FALSE))
+
   tracker <- dplyr::tibble(
-    date = as.Date(dplyr::first(games$date)),
+    date = as.Date(last_year$date),
     team = ifelse(
       last_year$home_final_score > last_year$away_final_score,
       last_year$home_name,
@@ -64,36 +71,40 @@ if(nrow(games) > 0){
       last_year$away_final_score,
       last_year$home_final_score
     ),
+    home_team = last_year$home_name,
     mov = team_score - opponent_score
   )
-  
-  for(i in 1:nrow(games)){
-    
+
+  for (i in 1:nrow(games)) {
     last_winner <- dplyr::last(tracker$team)
-    
+
     last_game <- double_games |>
-      dplyr::filter(team == last_winner & win == 0 & date > dplyr::last(tracker$date)) |>
+      dplyr::filter(team == last_winner & date > dplyr::last(tracker$date)) |>
       dplyr::slice(1)
-    
+
     tracker <- dplyr::bind_rows(
       tracker,
       dplyr::tibble(
         date = as.Date(last_game$date),
-        team = last_game$opponent,
-        opponent = last_game$team,
-        team_score = last_game$opponent_score,
-        opponent_score = last_game$team_score,
-        mov = team_score - opponent_score
+        team = dplyr::if_else(last_game$win, last_game$team, last_game$opponent),
+        opponent = dplyr::if_else(last_game$win, last_game$opponent, last_game$team),
+        team_score = dplyr::if_else(last_game$win, last_game$team_score, last_game$opponent_score),
+        opponent_score = dplyr::if_else(last_game$win, last_game$opponent_score, last_game$team_score),
+        home_team = last_game$home_team,
+        mov = team_score - opponent_score,
+        cup_change = !last_game$win
       )
     )
-    
+
     new_sched <- double_games |>
       dplyr::filter(team == dplyr::last(tracker$team) & date > dplyr::last(tracker$date))
-    
-    if(0 %in% new_sched$win & nrow(new_sched > 0)){
+
+    if (0 %in% new_sched$win & nrow(new_sched > 0)) {
       next
-    } else {break}
+    } else {
+      break
+    }
   }
-  
-  tracker |> saveRDS(paste0("data/transitive_tracker_",current_season-1,"_",substr(current_season,3,4),".rds"))
+
+  tracker |> saveRDS(paste0("data/transitive_tracker_an_", current_season - 1, "_", substr(current_season, 3, 4), ".rds"))
 }
